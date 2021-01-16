c3e3ba9d78a144715b59a04b18149c1d90572f9b
Justin Aiello <justinaiello@gmail.com>
2021-01-16 16:43:39 -0500

    feat(app): add styled components wrapper to app

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,14 +1,21 @@
+import { ThemeProvider } from 'styled-components';
+
 import './App.css';
+import theme from '../../styles/theme';
+import GlobalStyle from '../../styles/GlobalStyle.styled';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Run &apos;yarn storybook&apos; to view the component storybook
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Run &apos;yarn storybook&apos; to view the component storybook
         </p>
-      </header>
-    </div>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
